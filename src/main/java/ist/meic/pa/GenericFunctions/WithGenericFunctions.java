9651f85e758e5cd7bb284e9d1a639ae1df6cc37a
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-04-05 12:27:25 +0100

    Make main method handle control to next class in arguments

--- src/main/java/ist/meic/pa/GenericFunctions/WithGenericFunctions.java
+++ src/main/java/ist/meic/pa/GenericFunctions/WithGenericFunctions.java
@@ -1,7 +1,29 @@
 package ist.meic.pa.GenericFunctions;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class WithGenericFunctions {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // TODO: Stuff and thangs
+
+        // Handle control to next class in args
+        try {
+            Method newMain = Class.forName(args[0]).getMethod("main", String[].class);
+
+            // Get the new parameters for the class we're about to hand over control to
+            String[] parameters = Arrays.copyOfRange(args, 1, args.length);
+
+            // Hand over control, providing the right arguments
+            newMain.invoke(null, parameters);
+
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("No main class was specified! Can't hand over control, ending.");
+            throw e;
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            // Can't really do anything about this, let's just end the program
+            System.exit(-1);
+        }
     }
 }
