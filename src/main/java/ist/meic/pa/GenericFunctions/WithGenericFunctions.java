6c7df2856e7216a2b17daec248fb436180ce25c0
Nuno Gonçalves <nunombg@gmail.com>
2018-04-12 11:32:28 +0100

    Use javassist classpool to get the specified class

--- src/main/java/ist/meic/pa/GenericFunctions/WithGenericFunctions.java
+++ src/main/java/ist/meic/pa/GenericFunctions/WithGenericFunctions.java
@@ -1,16 +1,28 @@
 package ist.meic.pa.GenericFunctions;
 
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.NotFoundException;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class WithGenericFunctions {
-    public static void main(String[] args) {
-        // TODO: Stuff and thangs
+    public static void main(String[] args) throws CannotCompileException {
 
         // Handle control to next class in args
         try {
-            Method newMain = Class.forName(args[0]).getMethod("main", String[].class);
+            ClassPool cp = ClassPool.getDefault();
+
+            CtClass ct = cp.get(args[0]);
+
+            //TODO: changes to ctclass should go around here
+
+            Class c = ct.toClass();
+
+            Method newMain = c.getMethod("main", String[].class);
 
             // Get the new parameters for the class we're about to hand over control to
             String[] parameters = Arrays.copyOfRange(args, 1, args.length);
@@ -21,7 +33,7 @@ public class WithGenericFunctions {
         } catch(ArrayIndexOutOfBoundsException e) {
             System.out.println("No main class was specified! Can't hand over control, ending.");
             throw e;
-        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | NotFoundException e) {
             // Can't really do anything about this, let's just end the program
             System.exit(-1);
         }
