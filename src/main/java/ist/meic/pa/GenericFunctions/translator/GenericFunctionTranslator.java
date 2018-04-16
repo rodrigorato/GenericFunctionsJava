1589b79dc26613a3ac597b3f10b8c9a450db0af5
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-04-16 18:03:48 +0100

    Filter for annotated classes in translator

--- src/main/java/ist/meic/pa/GenericFunctions/translator/GenericFunctionTranslator.java
+++ src/main/java/ist/meic/pa/GenericFunctions/translator/GenericFunctionTranslator.java
@@ -1,9 +1,9 @@
 package ist.meic.pa.GenericFunctions.translator;
 
-import javassist.CannotCompileException;
-import javassist.ClassPool;
-import javassist.NotFoundException;
-import javassist.Translator;
+import ist.meic.pa.GenericFunctions.GenericFunction;
+import javassist.*;
+
+import java.util.Arrays;
 
 /**
  * This will inject code into the loaded classes, according to it's annotations
@@ -11,11 +11,24 @@ import javassist.Translator;
 public class GenericFunctionTranslator implements Translator {
     @Override
     public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
-
+        // We don't need to do anything here, for now
     }
 
     @Override
     public void onLoad(ClassPool pool, String classname) throws NotFoundException, CannotCompileException {
+        // First, check if the class has out GenericFunction annotation
+        CtClass loadedClass = pool.get(classname);
+        try {
+            if(Arrays.stream(loadedClass.getAnnotations())
+                    .anyMatch(p -> p instanceof GenericFunction)) {
+                // Apply our instrumentation!
+
+            }
+        } catch (ClassNotFoundException e) {
+            // TODO: handle this? is this ever going to happen?
+            e.printStackTrace();
+        }
+
 
     }
 }
