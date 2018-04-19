99557c9db581643973e27c990b8d07a835cf1f72
Nuno Gonçalves <nunombg@gmail.com>
2018-04-19 23:48:55 +0100

    Add support for before and after methods. Still has some bugs for now

--- src/main/java/ist/meic/pa/GenericFunctions/injectors/GenericCallInjector.java
+++ src/main/java/ist/meic/pa/GenericFunctions/injectors/GenericCallInjector.java
@@ -11,6 +11,8 @@ import javassist.NotFoundException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * This class is responsible for injecting code into the loaded classes,
@@ -18,6 +20,9 @@ import java.lang.reflect.Method;
  */
 public class GenericCallInjector implements AbstractInjector {
     private String callBackFunctionName = "callBack";
+    public static boolean invokedSuccessful = false;
+    public static boolean isSetup = false;
+    public static boolean beforeDone = false;
 
     @Override
     public void injectCode(CtClass ctClass) {
@@ -25,8 +30,8 @@ public class GenericCallInjector implements AbstractInjector {
         try {
             for(CtMethod m : ctClass.getDeclaredMethods()) {
                 if(!m.getName().equals(callBackFunctionName)) {
-                        m.insertBefore(
-                                generateCallBackFunctionCall(m.getLongName(), m.getReturnType().getName()));
+                    m.insertBefore(
+                            generateCallBackFunctionCall(m.getLongName(), m.getReturnType().getName()));
                 }
             }
 
@@ -39,18 +44,28 @@ public class GenericCallInjector implements AbstractInjector {
 
     @SuppressWarnings("unused")
     public static Object callBack(Class originalClass, String methodLongName, Object[] originalArgs) {
-        /*
-        // Call the respective before methods, from the most specific to the least one
-        if(!isSetupMethod()) {
 
-            doBeforeMethods();
+        if(!isSetup && !beforeDone) { // check if this is not a setup method call and if we already did the before methods for this call
+            doBeforeMethods(originalClass, originalArgs);
         }
-        */
+
+        beforeDone = true;
+
+        invokedSuccessful = false;
 
         try {
             Method best = findBestMethod(originalClass, originalArgs);
+            if (best == null)
+                return null;
             if(!methodLongName.equals(getLongNameFromMethod(best))) {
-                return best.invoke(null, originalArgs);
+                best.setAccessible(true); // take care of private methods
+                Object result = best.invoke(null, originalArgs);
+                invokedSuccessful = true;
+                if(!isSetup) {
+                    doAfterMethods(originalClass, originalArgs);
+                }
+                beforeDone = false;
+                return result;
             }
 
         } catch (Exception e) {
@@ -59,12 +74,6 @@ public class GenericCallInjector implements AbstractInjector {
 
         return null;
 
-        /*
-        // Call the respective after methods, from the least specific to the most one
-        if(!isSetupMethod) {
-            doAfterMethods();
-        }
-        */
     }
 
     private static String getLongNameFromMethod(Method m) {
@@ -72,12 +81,96 @@ public class GenericCallInjector implements AbstractInjector {
         return names[names.length -1];
     }
 
+    private static void doBeforeMethods(Class originalClass, Object[] originalArgs){
+
+        Method[] methods = originalClass.getDeclaredMethods();
+
+        LinkedList<Method> beforeMethods = new LinkedList<>();
+
+        for (Method candidate : methods){
+            if(MethodUtils.isMethodApplicable(candidate,originalArgs) && candidate.isAnnotationPresent(BeforeMethod.class)){
+                if(beforeMethods.isEmpty()){
+                    beforeMethods.add(candidate);
+                }
+                else{
+                    boolean added = false;
+                    for(int i = 0; i < beforeMethods.size(); i++){
+                        if(MethodUtils.isMethodMoreSpecific(candidate,beforeMethods.get(i))){
+                            beforeMethods.add(i,candidate);
+                            break;
+                        }
+                    }
+                    if(!added){
+                        beforeMethods.addLast(candidate);
+                    }
+                }
+            }
+        }
+
+        isSetup = true;
+        for (Method m : beforeMethods) {
+            m.setAccessible(true);
+
+            try {
+                m.invoke(null,originalArgs);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        isSetup = false;
+    }
+
+    private static void doAfterMethods(Class originalClass, Object[] originalArgs){
+
+        Method[] methods = originalClass.getDeclaredMethods();
+
+        LinkedList<Method> afterMethods = new LinkedList<>();
+
+        for (Method candidate : methods){
+            if(MethodUtils.isMethodApplicable(candidate,originalArgs) && candidate.isAnnotationPresent(AfterMethod.class)){
+                if(afterMethods.isEmpty()){
+                    afterMethods.add(candidate);
+                }
+                else{
+                    boolean added = false;
+                    for(int i = 0; i < afterMethods.size(); i++){
+                        if(MethodUtils.isMethodMoreSpecific(afterMethods.get(i),candidate)){
+                            afterMethods.add(i,candidate);
+                            break;
+                        }
+                    }
+                    if(!added){
+                        afterMethods.addLast(candidate);
+                    }
+                }
+            }
+        }
+
+        isSetup = true;
+        for (Method m : afterMethods) {
+
+            m.setAccessible(true);
+
+            try {
+                m.invoke(null,originalArgs);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        isSetup = false;
+
+    }
+
     public static Method findBestMethod(Class originalClass, Object[] args) {
         Method[] allMethods = originalClass.getDeclaredMethods();
 
         Method bestMethod = null;
         for(Method candidate : allMethods) {
-            if(MethodUtils.isMethodApplicable(candidate, args)) {
+            if(MethodUtils.isMethodApplicable(candidate, args) && !isSetupMethod(candidate)) {
                 if (bestMethod == null ||
                         MethodUtils.isMethodMoreSpecific(candidate, bestMethod)) {
                     bestMethod = candidate;
@@ -89,19 +182,18 @@ public class GenericCallInjector implements AbstractInjector {
     }
 
     public static boolean isSetupMethod(Method m) {
-        return hasAnnotation(m, AfterMethod.class) || hasAnnotation(m, BeforeMethod.class);
-    }
-
-    public static boolean hasAnnotation(Method m, Class targetAnnotation) {
-        for(Annotation annotation : m.getAnnotations()) {
-            if(annotation.getClass().getCanonicalName().equals(targetAnnotation.getCanonicalName())) {
-                return true;
-            }
-        }
-        return false;
+        return m.isAnnotationPresent(AfterMethod.class) || m.isAnnotationPresent(BeforeMethod.class);
     }
 
     private String generateCallBackFunctionCall(String methodName, String returnClassName) {
+        if (returnClassName.equals("void")){
+            return "if (!ist.meic.pa.GenericFunctions.injectors.GenericCallInjector.isSetup){" +
+                    "ist.meic.pa.GenericFunctions.injectors.GenericCallInjector" +
+                    "." + callBackFunctionName + "($class, \"" + methodName + "\", $args);" +
+                    "if(ist.meic.pa.GenericFunctions.injectors.GenericCallInjector.invokedSuccessful){" +
+                    "   return;" +
+                    "}}";
+        }
         return "Object ret = ist.meic.pa.GenericFunctions.injectors.GenericCallInjector" +
                 "." + callBackFunctionName + "($class, \"" + methodName + "\", $args);" +
                 "if(ret != null) {" +
