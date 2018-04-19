0b2dbf0a6d60aee7fd9456780623b856ffe458b7
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-04-19 13:54:44 +0100

    Add WIP garbage

--- src/main/java/ist/meic/pa/GenericFunctions/injectors/GenericCallInjector.java
+++ src/main/java/ist/meic/pa/GenericFunctions/injectors/GenericCallInjector.java
@@ -1,11 +1,17 @@
 package ist.meic.pa.GenericFunctions.injectors;
 
+import ist.meic.pa.GenericFunctions.AfterMethod;
+import ist.meic.pa.GenericFunctions.BeforeMethod;
 import javassist.CannotCompileException;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.CtNewMethod;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * This class is responsible for injecting code into the loaded classes,
@@ -20,7 +26,7 @@ public class GenericCallInjector implements AbstractInjector {
         try {
             for(CtMethod m : ctClass.getDeclaredMethods()) {
                 if(!m.getName().equals(callBackFunctionName)) {
-                        m.insertBefore(generateCallBackFunctionCall());
+                        m.insertBefore(generateCallBackFunctionCall(m.getName()));
                 }
             }
 
@@ -30,20 +36,51 @@ public class GenericCallInjector implements AbstractInjector {
     }
 
     @SuppressWarnings("unused")
-    public static void callBack(Class originalClass, Object[] originalArgs) {
+    public static void callBack(Class originalClass, String methodName, Object[] originalArgs) {
+        /*
+        // Call the respective before methods, from the most specific to the least one
+        if(!isSetupMethod()) {
+
+            doBeforeMethods();
+        }
+        */
+
+
         Method[] declaredMethods = originalClass.getDeclaredMethods();
         for (Method method : declaredMethods) {
-            for (Class parameterType : method.getParameterTypes()) {
-                System.out.println(parameterType.getName());
+
+            if(!isSetupMethod(method)) {
+                for (Class parameterType : method.getParameterTypes()) {
+                    System.out.println(parameterType.getName());
+                }
+                System.out.println("");
             }
-            System.out.println("");
         }
 
+        /*
+        // Call the respective after methods, from the least specific to the most one
+        if(!isSetupMethod) {
+            doAfterMethods();
+        }
+        */
     }
 
-    private String generateCallBackFunctionCall() {
+    public static boolean isSetupMethod(Method m) {
+        return hasAnnotation(m, AfterMethod.class) || hasAnnotation(m, BeforeMethod.class);
+    }
+
+    public static boolean hasAnnotation(Method m, Class targetAnnotation) {
+        for(Annotation annotation : m.getAnnotations()) {
+            if(annotation.getClass().getCanonicalName().equals(targetAnnotation.getCanonicalName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String generateCallBackFunctionCall(String methodName) {
         return "ist.meic.pa.GenericFunctions.injectors.GenericCallInjector" +
-                "." + callBackFunctionName + "($class, $args);";
+                "." + callBackFunctionName + "($class, \"" + methodName + "\", $args);";
     }
 
 }
