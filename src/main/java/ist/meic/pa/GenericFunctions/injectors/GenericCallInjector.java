75ec408fc2982193793de3d5eb9f1da16ce75cf6
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-04-19 10:43:16 +0100

    Refactor code injection into actual method

--- src/main/java/ist/meic/pa/GenericFunctions/injectors/GenericCallInjector.java
+++ src/main/java/ist/meic/pa/GenericFunctions/injectors/GenericCallInjector.java
@@ -18,7 +18,6 @@ public class GenericCallInjector implements AbstractInjector {
     public void injectCode(CtClass ctClass) {
 
         try {
-            addCallBackFunction(ctClass);
             for(CtMethod m : ctClass.getDeclaredMethods()) {
                 if(!m.getName().equals(callBackFunctionName)) {
                         m.insertBefore(generateCallBackFunctionCall());
@@ -30,40 +29,20 @@ public class GenericCallInjector implements AbstractInjector {
         }
     }
 
-    private String generateCallBackFunctionCode() {
-        String code = "public static void " + callBackFunctionName + "(Class originalClass, Object[] originalArgs) {" +
-                "   /*System.out.println(\"Hello world!\"); " +
-                "   for(int i = 0; i < originalArgs.length; i++) {" +
-                "       System.out.println(originalArgs[i].getClass().getName()); " +
-                "   }" +
-                "   System.out.println(originalClass.getDeclaredMethods());*/" +
-                "" +
-                "   java.lang.reflect.Method[] declaredMethods = originalClass.getDeclaredMethods();" +
-                "   for(int m = 0; m < declaredMethods.length; m++) {" +
-                "       java.lang.reflect.Method method = declaredMethods[m];" +
-                "       for(int a = 0; a < method.getParameterTypes().length; a++) {" +
-                "           Class parameterType = method.getParameterTypes()[a];" +
-                "           System.out.println(parameterType.getName());" +
-                "       }" +
-                "       System.out.println(\"\");" +
-                "   }";
-
-                code += "}";
-
-        return code;
-    }
-
-    private String generateCallBackFunctionCall() {
-        return callBackFunctionName + "($class, $args);";
-    }
-
-    private void addCallBackFunction(CtClass ctClass) {
-        try {
-            CtMethod m = CtNewMethod.make(generateCallBackFunctionCode(), ctClass);
-            ctClass.addMethod(m);
-        } catch (CannotCompileException e) {
-            e.printStackTrace();
+    public static void callBack(Class originalClass, Object[] originalArgs) {
+        Method[] declaredMethods = originalClass.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            for (Class parameterType : method.getParameterTypes()) {
+                System.out.println(parameterType.getName());
+            }
+            System.out.println("");
         }
 
     }
+
+    private String generateCallBackFunctionCall() {
+        return "ist.meic.pa.GenericFunctions.injectors.GenericCallInjector" +
+                ".callBack($class, $args);";
+    }
+
 }
