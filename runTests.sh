1430ff54b19f23c6941d30766992c0c1f37e230f
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-04-19 16:12:03 +0100

    Fix directories on run tests script

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TESTS_DIR=./tests_outputs/
+
 # Compile as described in the project guide
 gradle compileJava build
 
@@ -8,10 +10,10 @@ for t in src/main/java/ist/meic/pa/GenericFunctions/examples/Test*.java; do
     test_name=$(basename $t .java)
     test_letter=$(echo $test_name | sed -r 's/Test//g')
 
-    gradle clean run -PrunArgs='ist.meic.pa.GenericFunctions.examples.'$test_name -q | grep -v "Note:\ " > ./tests/$test_letter.my_out
+    gradle clean run -PrunArgs='ist.meic.pa.GenericFunctions.examples.'$test_name -q | grep -v "Note:\ " > $TESTS_DIR$test_letter.my_out
 
     echo "Diff on "$test_name":"
-    colordiff --strip-trailing-cr ./tests/$test_letter.my_out ./tests/$test_letter.out
+    colordiff --strip-trailing-cr $TESTS_DIR$test_letter.my_out $TESTS_DIR$test_letter.out
 
 done
 
